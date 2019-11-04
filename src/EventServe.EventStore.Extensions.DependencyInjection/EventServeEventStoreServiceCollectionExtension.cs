65b05877a1229ba05610986b2ed7156c4a81ca2c
Rami Labana <admin@cart.supply>
2019-11-03 20:30:48 -0500

    Add a dependency injection extension project

---
+++ src/EventServe.EventStore.Extensions.DependencyInjection/EventServeEventStoreServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using EventServe.EventStore.Interfaces;
+using EventServe.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EventServe.EventStore.Extensions.DependencyInjection
+{
+    public static class EventServeEventStoreServiceCollectionExtension
+    {
+        public static void AddEventServeEventStore(this IServiceCollection services, Action<EventStoreConnectionOptions> setupAction)
+        {
+            services.Configure(setupAction);
+            services.AddScoped<IEventStoreConnectionProvider, EventStoreConnectionProvider>();
+            services.AddScoped<IEventStreamReader, EventStoreStreamReader>();
+            services.AddScoped<IEventStreamWriter, EventStoreStreamWriter>();
+            services.AddScoped<IEventSerializer, EventSerializer>();
+        }
+    }
+}
