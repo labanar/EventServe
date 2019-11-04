10515a43cf8613a1bbdbc1383072ef42e4eaa217
Rami Labana <admin@cart.supply>
2019-11-03 20:58:06 -0500

    Add a basic write aggregate test

--- tests/Integration/EventServe.EventStore.IntegrationTests/EventStoreRepositoryShould.cs
+++ tests/Integration/EventServe.EventStore.IntegrationTests/EventStoreRepositoryShould.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using EventServe.Services;
+using FluentAssertions;
 using Microsoft.Extensions.Options;
 using Xunit;
 
@@ -20,6 +22,27 @@ namespace EventServe.EventStore.IntegrationTests {
         [Fact]
         public async Task Writes_aggregate_to_stream() {
 
+            var resetCommand = new ResetDummyAggregateCommand() {
+                Id = Guid.NewGuid(),
+                Name = "The name",
+                Url = "https://url.example.com"
+            };
+
+            var aggregate = new DummyAggregate(resetCommand);
+
+            var sut = CreateRepository<DummyAggregate>();
+            var numberOfChanges = await sut.SaveAsync(aggregate);
+
+            var reader = CreateStreamReader();
+            var stream = StreamBuilder.Create()
+                .WithAggregateType<DummyAggregate>()
+                .WithAggregateId(resetCommand.Id)
+                .Build();
+
+            aggregate.Version.Should().Be(0);
+
+            var events = await reader.ReadAllEventsFromStream(stream.Id);
+            events.Count.Should().Be(1);
         }
 
         private EventRepository<T> CreateRepository<T>()
@@ -29,9 +52,14 @@ namespace EventServe.EventStore.IntegrationTests {
             var reader = new EventStoreStreamReader(connectionProvider, _serializer);
             var writer = new EventStoreStreamWriter(connectionProvider, _serializer);
 
-            var sut = new EventRepository<T>(writer, reader, null);
+            var sut = new EventRepository<T>(writer, reader);
 
             return sut;
+        }
+
+        private IEventStreamReader CreateStreamReader() {
+            var connectionProvider = new EventStoreConnectionProvider(Options.Create(_fixture.EventStoreConnectionOptions));
+            return new EventStoreStreamReader(connectionProvider, _serializer);
         }
 
     }
