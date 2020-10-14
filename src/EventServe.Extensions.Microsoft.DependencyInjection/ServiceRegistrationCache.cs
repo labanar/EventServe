a285b3f00c60925bc993094dfd99e58bdcf430fb
Rami Labana <labanar@live.com>
2020-10-14 11:08:44 -0400

    Fix a bug with the registration of multiple partitioned projection profiles

---
+++ src/EventServe.Extensions.Microsoft.DependencyInjection/ServiceRegistrationCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventServe.Extensions.Microsoft.DependencyInjection
+{
+    public static class ServiceRegistrationCache
+    {
+        public static List<Type> ProjectionProfileTypes = new List<Type>();
+    }
+}
